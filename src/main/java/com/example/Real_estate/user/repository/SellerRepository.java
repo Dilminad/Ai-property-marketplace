73a61c397f973c3774ef9646d8193456e5320039
Dilmina <dilmina88@gmail.com>
2026-07-12 23:37:40 +0530

    created seller repo

--- src/main/java/com/example/Real_estate/user/repository/SellerRepository.java
+++ src/main/java/com/example/Real_estate/user/repository/SellerRepository.java
@@ -1,5 +1,31 @@
 package com.example.Real_estate.user.repository;
 
-public interface SellerRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Real_estate.user.entity.Seller;
+import java.util.List;
+
+
+public interface SellerRepository extends JpaRepository<Seller, Long >{
+
+    //find by email
+    Optional<Seller>findByEmail (String email);
+
+    //find by username
+    Optional<Seller>findByUsername (String userName);
+
+    //find by phone
+    Optional <Seller> findByPhone(String phone );
+
+    //check existance
+    boolean existsByEmail(String email);
+    boolean existsByUserName(String userName);
+    boolean existsByPhone(String phone);
+
+    //find by type
+    List<Seller>findBySellerType (String sellerType);
+
+    //find activ
 }
